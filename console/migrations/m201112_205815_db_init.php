21560cd05d0c5f89ce99a810d4a6fdf5822a25d1
Alexey Ivanov <alexeyivanov@yandex.ru>
2020-11-12 22:40:49 +0000

    Try to fix something

--- console/migrations/m201112_205815_db_init.php
+++ console/migrations/m201112_205815_db_init.php
@@ -25,11 +25,37 @@ class m201112_205815_db_init extends Migration
         return false;
     }
 
-    /*
     // Use up()/down() to run migration code without a transaction.
     public function up()
     {
+        $this->createTable('manager', [
+            'id' => $this->primaryKey(),
+            'firstname' => $this->string(),
+            'lastname' => $this->string(),
+            'password' => $this->string(),
+            'login' => $this->string(),
+            'email' => $this->string(),
+            'phone' => $this->string(),
+            'status' => $this->integer()
+        ]);
+        
+        $this->addColumn('user', 'phone', $this->string());
+        
+        $this->createTable('call', [
+            'client_id' => $this->integer(),
+            'manager_id' => $this->integer(),
+            'start_time' => $this->time(),
+            'status' => $this->integer()
+        ]);
+        $this->addForeignKey('fk-call-client_id', 'call', 'client_id', 'user', 'id', 'CASCADE');
+        $this->addForeignKey('fk-call-manager_id', 'call', 'manager_id', 'manager', 'id', 'CASCADE');
 
+        $this->createTable('course', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'link_to_video' => $this->string(),
+            'price' => $this->money(),
+        ]);
     }
 
     public function down()
@@ -38,5 +64,4 @@ class m201112_205815_db_init extends Migration
 
         return false;
     }
-    */
 }
